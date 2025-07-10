e62342a187477134288a6a115f9cc49a32599cfa
mfachryna <fachry.arifna@gmail.com>
2025-07-10 18:33:35 +0700

    fix (Navbar): Fix resume url 404

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,7 +3,7 @@ import { RESUME_URL } from '$env/static/private';
 export async function load() {
     return {
         resumeUrl: RESUME_URL 
-            ? `RESUME_URL`
+            ? RESUME_URL
             : null
     };
 }
