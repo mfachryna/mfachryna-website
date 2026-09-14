2c70b3c93d2a2df44cbaacd4e7c9cb22d5b8c7ed
mfachryna <fachry.arifna@gmail.com>
2026-09-14 20:44:02 +0700

    perf: set vercel region to sin1 and add database query indexes

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			regions: ['sin1']
+		})
+	}
 };
 
 export default config;
